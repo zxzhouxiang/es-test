e37a09d04d42e8b6f235514b7bd6a630dba9055a
zxzhouxiang <zxzhouxiang123@163.com>
2016-10-18 11:25:06 +0800

    commit

--- src/main/java/ESearch.java
+++ src/main/java/ESearch.java
@@ -1,7 +1,10 @@
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
 
 import java.net.InetAddress;
 import java.util.Map;
@@ -14,8 +17,15 @@ public class ESearch {
         Settings settings = Settings.settingsBuilder().put("cluster.name", "ink-es").build();
         TransportClient client = TransportClient.builder().settings(settings).build()
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.1.5.139"), 9300));
-        GetResponse response= client.prepareGet("twitter","tweet","1").get();
-        Map map= response.getSource();
+       // GetResponse response= client.prepareGet("twitter","tweet","1").get();
+        SearchResponse response= client.prepareSearch("twitter").setTypes("tweet")
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(QueryBuilders.termQuery("user", "kimchy"))                 // Query
+                //.setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))     // Filter
+                //.setFrom(0).setSize(60).setExplain(true)
+                .execute()
+                .actionGet();
+        Map map= response.getHits().getAt(0).getSource();
         System.out.println(map.toString());
 
     }
